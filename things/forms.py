0e0c7a0fdacbc1a95efbeb60c026abf4fc8c2da2
heliip812 <heliip812@gmail.com>
2022-12-08 13:38:29 +0000

    amend

--- things/forms.py
+++ things/forms.py
@@ -13,24 +13,9 @@ class ThingForm(forms.ModelForm):
         model = Thing
         fields = ['name', 'description', 'quantity']
 
-    name = forms.CharField(
-        validators=[
-            MaxLengthValidator(35, message='name cannot be more than 50 characters')
-        ]
-    )
-
-    description = forms.Textarea(
-        validators=[
-            MaxLengthValidator(120, message='name cannot be more than 50 characters')
-        ]
-    )
-
-    quantity = forms.NumberInput(
-        validators=[
-            MinValueValidator(0, message='quantity cannot be less than 0'),
-            MaxValueValidator(50, message='quantity cannot be larger than 50')
-        ]
-    )
+    name = forms.CharField()
+    description = forms.Textarea()
+    quantity = forms.NumberInput()
 
     def save(self):
         """Create a new user."""
