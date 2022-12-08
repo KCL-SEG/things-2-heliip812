ec553b6ee0a92d6c4746e593c1a53ec125a10866
heliip812 <heliip812@gmail.com>
2022-12-08 13:55:29 +0000

    amend

--- things/forms.py
+++ things/forms.py
@@ -1,29 +1,16 @@
 """Forms of the project."""
 from django import forms
-from django.core.validators import MinValueValidator, MaxValueValidator, MaxLengthValidator
 from .models import Thing
 
 # Create your forms here.
 
 class ThingForm(forms.ModelForm):
 
-    # class Meta:
-    #     """Form options."""
+    class Meta:
+        """Form options."""
 
-    #     model = Thing
-    #     fields = ['name', 'description', 'quantity']
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {"description":forms.Textarea(), "quantity": forms.NumberInput()}
 
-    name = forms.CharField()
-    description = forms.Textarea()
-    quantity = forms.NumberInput()
 
-    def save(self):
-        """Create a new user."""
-        super().clean()
-        super().save(commit=False)
-        thing = Thing.objects.create(
-            name=self.cleaned_data.get('name'),
-            description=self.cleaned_data.get('description'),
-            quantity=self.cleaned_data.get('quantity'),
-        )
-        return thing
--- things/views.py
+++ things/views.py
@@ -3,10 +3,6 @@ from .forms import ThingForm
 
 def home(request):
 
-    if request.method == 'POST':
-        form = ThingForm(request.POST)
-        if form.is_valid():
-            thing = form.save()
-    else:
-        form = ThingForm()
+    form = ThingForm()
+       
     return render(request, 'home.html', {'form': form})
